d6f778cdc945ab49f2b47a6389b7918ae0b07d54
Tagirov_zakir <zakir0556@gmail.com>
2021-05-30 18:21:22 +0500

    дробные числа

--- js/main.js
+++ js/main.js
@@ -11,4 +11,24 @@ function getRandomNum(first, second) {
   return result;
 }
 
+function getRandomNumFloat(first, second, parce = 0) {
+  let min;
+  let max;
+  let result;
+  if (!Number.isFinite(first) || first < 0 || !Number.isFinite(second) || second < 0 || !Number.isFinite(parce) || parce < 0) {
+    return result = 'одно или оба значения не являются конечным числом, либо меньше нуля';
+  }
+  parce = Math.floor(parce); // делаем число знаков после запятой целым
+  first > second ? (min = second, max = first) : (min = first, max = second);
+  max = Math.floor(max * 10 ** parce);
+  min = Math.ceil(min * 10 ** parce);
+  if (max < min) {
+    return result = 'в указанном диапазон нет чисел с необходимым количеством знаков после запятой';
+  }
+  result = Math.floor((Math.random() * (max - min + 1)) + min); // если a === b, всегда выводится это число
+  // result = result.toFixed(parce);
+  return result / 10 ** parce;
+}
+
 getRandomNum(1, 5);
+getRandomNumFloat(1, 5, 2);
