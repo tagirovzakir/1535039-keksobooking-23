67950a895cdec377b5604580876a22d2db1654f5
Tagirov_zakir <zakir0556@gmail.com>
2021-06-21 00:42:36 +0500

    Формы, поправил

--- js/form.js
+++ js/form.js
@@ -1,17 +1,14 @@
 export const deactivateForms = function (forms) {
   forms.forEach((form) => {
-    const formClassName = form.className;
-    if (!form.classList.contains(`${formClassName}--disabled`)) { form.classList.add(`${formClassName}--disabled`); }
-    Array.from(form.children).forEach((formElement) => {
-      formElement.setAttribute('disabled', 'true');
-    });
+    const hidingClass = `${form.dataset.hidingClass}--disabled`;
+    if (!form.classList.contains(hidingClass)) {form.classList.add(hidingClass);}
+    Array.from(form.children).forEach((formElement) => formElement.disabled = 'true');
   });
 };
 export const activateForms = function (forms) {
   forms.forEach((form) => {
-    form.classList.remove(`${form.classList[1]}`);
-    Array.from(form.children).forEach((formElement) => {
-      formElement.removeAttribute('disabled');
-    });
+    const hidingClass = `${form.dataset.hidingClass}--disabled`;
+    if (form.classList.contains(hidingClass)) {form.classList.remove(hidingClass);}
+    Array.from(form.children).forEach((formElement) => formElement.disabled = '');
   });
 };
